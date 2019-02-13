b7be9a0068cab44ac8477b49e13875e517681895
meriadec <meriadec.pillet@gmail.com>
2019-02-13 14:23:08 +0100

    disable some stuff

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,11 +8,6 @@ source scripts/helpers/run-job.sh
 # shellcheck disable=SC1091
 source scripts/helpers/display-env.sh
 
-if [ "$(git rev-parse --abbrev-ref HEAD)" != "master" ]; then
-  echo "You are not on master. Exiting properly. (CI)"
-  exit 0
-fi
-
 GH_TAG=$(git describe --exact-match --tags 2>/dev/null || echo '')
 
 if [[ $GH_TAG == "" ]]; then
@@ -22,34 +17,6 @@ fi
 
 if [ -z "$GH_TOKEN" ]; then
   echo "GH_TOKEN is unset. can't release" >&2
-  exit 1
-fi
-
-if [ ! -d "static/fonts/museosans" ]; then
-  if ! command -v aws ; then
-    if ! command -v apt ; then
-      echo "Museo Sans is missing, and I can't fetch it (no aws, no apt)" >&2
-      exit 1
-    fi
-
-    runJob "sudo apt install awscli" "installing aws cli..." "installed aws cli" "failed to install aws cli"
-  fi
-
-  runJob \
-    "set -e ;\
-    rm -rf /tmp/museosans* ;\
-    aws s3 cp s3://ledger-ledgerlive-resources-dev/resources/museosans.zip /tmp/museosans.zip ;\
-    unzip /tmp/museosans.zip -d /tmp/museosans ;\
-    mv /tmp/museosans/museosans static/fonts ;\
-    rm static/fonts/museosans/.DS_Store # remove crappy macOS file ;\
-    rm -rf /tmp/museosans*" \
-    "no museosans font. fetching it from private bucket..." \
-    "successfully fetched museosans" \
-    "error fetching museosans"
-fi
-
-if ! git diff-index --quiet HEAD --; then
-  echo "you have uncommitted local changes!" >&2
   exit 1
 fi
 
